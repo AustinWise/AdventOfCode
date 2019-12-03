3f5e31d5db1b7a6e6dfc53b6d831bb00bc399fbf
Austin Wise <AustinWise@gmail.com>
2019-12-02 19:02:23 -0800

    Do not allow negitive mass or negitive fuel.

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,13 +1,17 @@
 use std::error::Error;
 
-fn calculate_fuel_required(mass: i32) -> i32 {
-    mass / 3 - 2
+fn calculate_fuel_required(mass: u32) -> u32 {
+    if mass < 6 {
+        0
+    } else {
+        mass / 3 - 2
+    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut total : i32 = 0;
+    let mut total = 0;
     for line in std::fs::read_to_string("input.txt")?.lines() {
-        let mass = i32::from_str_radix(line, 10)?;
+        let mass = u32::from_str_radix(line, 10)?;
         total += calculate_fuel_required(mass);
     }
 
@@ -21,6 +25,7 @@ mod tests {
 
     #[test]
     fn test_sample_input() {
+        assert_eq!(0, calculate_fuel_required(0));
         assert_eq!(2, calculate_fuel_required(12));
         assert_eq!(2, calculate_fuel_required(14));
         assert_eq!(654, calculate_fuel_required(1969));
