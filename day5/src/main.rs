b6918306f47e9c966eb3e6f4e261f4136aeb2bb2
Austin Wise <AustinWise@gmail.com>
2019-12-25 20:42:10 +0900

    day5 part1

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,12 +1,19 @@
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt;
+use std::io::BufRead;
+use std::io::BufWriter;
+use std::io::Write;
 
 #[derive(Debug)]
 enum MyError {
     ProgramParseError,
+    InvalidParameterMode,
     InvalidOpCode,
     IndexOutOfRange,
+    EOF,
+    InputParse,
+    IoError(std::io::Error),
     AnswerNotFound,
 }
 
@@ -16,17 +23,21 @@ impl fmt::Display for MyError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             MyError::ProgramParseError => write!(f, "failed to parse program"),
+            MyError::InvalidParameterMode => write!(f, "invalid parameter mode"),
             MyError::InvalidOpCode => write!(f, "invalid opcode"),
             MyError::IndexOutOfRange => write!(f, "index out of range"),
+            MyError::EOF => write!(f, "EOF"),
+            MyError::InputParse => write!(f, "Input parse"),
+            MyError::IoError(io_err) => write!(f, "io error: {}", io_err),
             MyError::AnswerNotFound => write!(f, "answer not found"),
         }
     }
 }
 
-fn parse_program(input: &str) -> Result<Vec<u32>, MyError> {
-    let mut v: Vec<u32> = Vec::new();
+fn parse_program(input: &str) -> Result<Vec<i32>, MyError> {
+    let mut v: Vec<i32> = Vec::new();
     for num_str in input.split(',') {
-        if let Ok(num) = u32::from_str_radix(num_str, 10) {
+        if let Ok(num) = i32::from_str_radix(num_str, 10) {
             v.push(num);
         } else {
             return Err(MyError::ProgramParseError);
@@ -35,39 +46,127 @@ fn parse_program(input: &str) -> Result<Vec<u32>, MyError> {
     Ok(v)
 }
 
-fn get_usize(mem: &[u32], ndx: usize) -> Result<usize, MyError> {
+enum ParameterMode {
+    Position,
+    Immediate,
+}
+
+enum Opcode {
+    Add(ParameterMode, ParameterMode),
+    Multiply(ParameterMode, ParameterMode),
+    Input,
+    Output(ParameterMode),
+    Exit,
+}
+
+fn parse_parameter_mode(parameter_digit: i32) -> Result<ParameterMode, MyError> {
+    match parameter_digit {
+        0 => Ok(ParameterMode::Position),
+        1 => Ok(ParameterMode::Immediate),
+        _ => Err(MyError::InvalidParameterMode),
+    }
+}
+
+fn parse_instruction(instruction: i32) -> Result<Opcode, MyError> {
+    let ret = match instruction % 100 {
+        1 => Opcode::Add(
+            parse_parameter_mode(instruction / 100 % 10)?,
+            parse_parameter_mode(instruction / 1000 % 10)?,
+        ),
+        2 => Opcode::Multiply(
+            parse_parameter_mode(instruction / 100 % 10)?,
+            parse_parameter_mode(instruction / 1000 % 10)?,
+        ),
+        3 => Opcode::Input,
+        4 => Opcode::Output(parse_parameter_mode(instruction / 100 % 10)?),
+        99 => Opcode::Exit,
+        _ => return Err(MyError::InvalidOpCode),
+    };
+    let parameter_mode_count = match ret {
+        Opcode::Add(_, _) => 2,
+        Opcode::Multiply(_, _) => 2,
+        Opcode::Input => 0,
+        Opcode::Output(_) => 1,
+        Opcode::Exit => 0,
+    };
+    let max_value = (10i32).pow(2 + parameter_mode_count) - 1;
+    if instruction > max_value {
+        Err(MyError::InvalidOpCode)
+    } else {
+        Ok(ret)
+    }
+}
+
+fn get_usize(mem: &[i32], ndx: usize) -> Result<usize, MyError> {
     match usize::try_from(mem[ndx]) {
         Ok(num) => Ok(num),
         Err(_) => Err(MyError::IndexOutOfRange),
     }
 }
 
-fn execute(mem: &mut [u32]) -> Result<(), MyError> {
+fn load(mem: &[i32], index: usize, mode: ParameterMode) -> Result<i32, MyError> {
+    let ret = match mode {
+        ParameterMode::Position => mem[get_usize(mem, index)?],
+        ParameterMode::Immediate => mem[index],
+    };
+    Ok(ret)
+}
+
+fn read_number(input: &mut dyn BufRead, output: &mut dyn Write) -> Result<i32, MyError> {
+    write!(output, "Please enter a number: ").unwrap();
+    output.flush().unwrap();
+    let mut buf = String::new();
+    match input.read_line(&mut buf) {
+        Ok(0) => Err(MyError::EOF),
+        Err(io_err) => Err(MyError::IoError(io_err)),
+        Ok(bytes_read) => match i32::from_str_radix(&buf.trim(), 10) {
+            Ok(ret) => Ok(ret),
+            Err(err) => {Err(MyError::InputParse)
+            },
+        },
+    }
+}
+
+fn execute(
+    mem: &mut [i32],
+    input: &mut dyn BufRead,
+    output: &mut dyn Write,
+) -> Result<(), MyError> {
     let mut pc = 0;
     loop {
-        match mem[pc] {
-            1 => {
-                let src1 = get_usize(&mem, pc + 1)?;
-                let src2 = get_usize(&mem, pc + 2)?;
+        match parse_instruction(mem[pc])? {
+            Opcode::Add(src1_mode, src2_mode) => {
                 let dst = get_usize(&mem, pc + 3)?;
-                mem[dst] = mem[src1] + mem[src2];
+                mem[dst] = load(mem, pc + 1, src1_mode)? + load(mem, pc + 2, src2_mode)?;
                 pc += 4;
             }
-            2 => {
-                let src1 = get_usize(&mem, pc + 1)?;
-                let src2 = get_usize(&mem, pc + 2)?;
+            Opcode::Multiply(src1_mode, src2_mode) => {
                 let dst = get_usize(&mem, pc + 3)?;
-                mem[dst] = mem[src1] * mem[src2];
+                mem[dst] = load(mem, pc + 1, src1_mode)? * load(mem, pc + 2, src2_mode)?;
                 pc += 4;
             }
-            99 => return Ok(()),
-            _ => return Err(MyError::InvalidOpCode),
+            Opcode::Input => {
+                let dst = get_usize(&mem, pc + 1)?;
+                mem[dst] = read_number(input, output)?;
+                pc += 2;
+            }
+            Opcode::Output(src_mode) => {
+                writeln!(output, "{}\n", load(mem, pc + 1, src_mode)?);
+                pc += 2;
+            }
+            Opcode::Exit => return Ok(()),
         }
     }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mem = parse_program(&std::fs::read_to_string("input.txt")?)?;
+    let mut mem = parse_program(&std::fs::read_to_string("input.txt")?)?;
+    execute(
+        &mut mem,
+        &mut std::io::stdin().lock(),
+        &mut std::io::stdout().lock(),
+    )?;
+    println!("asdf");
     Ok(())
 }
 
@@ -86,7 +185,7 @@ mod tests {
     fn test_a_program(input: &str, expected_output: &str) {
         let mut mem = parse_program(input).expect("failed to parse input");
         let expected_mem = parse_program(expected_output).expect("failed to parse input");
-        execute(&mut mem).expect("execute failed");
+        execute(&mut mem, &mut std::io::empty(), &mut std::io::sink()).expect("execute failed");
         assert_eq!(mem, expected_mem);
     }
 
