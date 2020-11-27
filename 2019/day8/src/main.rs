0e09ffdac559f1bed765df52f44de92bf5815dd0
Austin Wise <AustinWise@gmail.com>
2020-11-26 21:08:00 -0800

    2019 day8 part2

--- 2019/day8/src/main.rs
+++ 2019/day8/src/main.rs
@@ -18,38 +18,40 @@ impl fmt::Display for ErrorCodes {
 
 impl Error for ErrorCodes {}
 
-fn parse_image_layers(width: usize, height: usize, input: &str) -> Result<Vec<Vec<u8>>, ErrorCodes>
-{
+fn parse_image_layers(
+    width: usize,
+    height: usize,
+    input: &str,
+) -> Result<Vec<Vec<u8>>, ErrorCodes> {
     let pixles_per_layer = width * height;
 
-    let mut all_bytes : Vec<u8> = vec!();
+    let mut all_bytes: Vec<u8> = vec![];
     for ch in input.chars() {
         if let Ok(num) = u8::from_str_radix(&ch.to_string(), 10) {
             all_bytes.push(num);
-        }
-        else {
+        } else {
             return Err(ErrorCodes::FileParseFailure);
         }
     }
 
-    Ok(all_bytes.chunks_exact(pixles_per_layer).map(|chunck| chunck.iter().map(|c| *c).collect::<Vec<u8>>()).collect())
+    Ok(all_bytes
+        .chunks_exact(pixles_per_layer)
+        .map(|chunck| chunck.iter().map(|c| *c).collect::<Vec<u8>>())
+        .collect())
 }
 
-struct ColorOccurences
-{
-    pub colors: [usize; 10]
+struct ColorOccurences {
+    pub colors: [usize; 10],
 }
 
 fn count_colors(layer: &Vec<u8>) -> ColorOccurences {
-    let mut colors : [usize; 10] = [0; 10];
+    let mut colors: [usize; 10] = [0; 10];
 
     for color in layer {
         colors[*color as usize] += 1;
     }
 
-    ColorOccurences {
-        colors: colors
-    }
+    ColorOccurences { colors: colors }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -62,7 +64,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut least_zeros = usize::MAX;
     let mut one_times_two = 0;
 
-    for layer in layers.iter().map(|l| count_colors(&l) ) {
+    for layer in layers.iter().map(|l| count_colors(&l)) {
         if layer.colors[0] < least_zeros {
             least_zeros = layer.colors[0];
             one_times_two = layer.colors[1] * layer.colors[2];
@@ -70,6 +72,34 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     println!("{}", one_times_two);
+
+    let decoded_images: Vec<u8> = vec![255; width * height]; //arbitrary high color
+    let decoded_images = layers.iter().rev().fold(decoded_images, |img, layer| {
+        return img
+            .iter()
+            .zip(layer.iter())
+            .map(|(c_img, c_layer)| match *c_layer {
+                0 => 0,
+                1 => 1,
+                2 => *c_img,
+                _ => panic!(),
+            })
+            .collect();
+    });
+
+    for line in decoded_images.chunks_exact(width) {
+        for ch in line.iter() {
+            print!(
+                "{}",
+                match ch {
+                    0 => ' ',
+                    1 => '#',
+                    _ => panic!(),
+                }
+            );
+        }
+        println!();
+    }
 
     Ok(())
 }
