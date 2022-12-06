3e3592279927f0101fe6ff8fa1223745688b25d1
Austin Wise <AustinWise@gmail.com>
2022-12-05 23:51:02 -0800

    2022 day 1 part 1

---
+++ 2022/day1/Program.cs
@@ -0,0 +1,15 @@
+﻿int max = 0;
+int cur = 0;
+foreach (var line in day1.Properties.Resources.Input.Split('\n').Select(l => l.Trim('\r')))
+{
+    if (line.Length == 0)
+    {
+        max = Math.Max(cur, max);
+        cur = 0;
+    }
+    else
+    {
+        cur += int.Parse(line);
+    }
+}
+Console.WriteLine(max);
