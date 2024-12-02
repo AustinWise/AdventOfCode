132afd30b6609a1c3fa201526ac64f10d2a8a532
Austin Wise <AustinWise@gmail.com>
2024-12-01 20:59:26 -0800

    2024 day 1 part 2

--- 2024/day1/Program.cs
+++ 2024/day1/Program.cs
@@ -25,4 +25,13 @@ for (int i = 0; i < leftList.Count; i++)
     sumOfDistances += Math.Abs(leftList[i] - rightList[i]);
 }
 
-Console.WriteLine(sumOfDistances);
+Console.WriteLine($"part1: {sumOfDistances}");
+
+// We want to find the number of times each element in the left list appears in the right.
+// Since we already sorted the right list, we could walk both lists at the same time, maintaining two indexes.
+// But that sounds annoying, so just count everything in right side first then look up the count for each in the left.
+
+var rightCounts = rightList.GroupBy(i => i).ToDictionary(g => g.Key, g => g.Count());
+
+int partTwoValue = leftList.Select(i => rightCounts.TryGetValue(i, out int value) ? value * i : 0).Sum();
+Console.WriteLine($"part2: {partTwoValue}");
