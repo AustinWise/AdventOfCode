561d6d1dde9ca801ed68507c1f6299090c41f46c
Austin Wise <AustinWise@gmail.com>
2024-12-17 17:01:51 -0800

    Ok, trying to rational numbers only and avoid angles is not working.
    The motivation for using rationals was to have precise representations of vectors that was immune to the problems of comparing floating point numbers. However I'm going through gymnastics to compare these rational numbers like vectors. And the values we are dealing with are on a less than 100x100 cartesian grid where all coordinates are integers. Rounding is not a big deal.
    
    Also this solution was posted to Reddit and it is so much cleaner:
    
    https://github.com/prscoelho/aoc2019/blob/master/src/aoc10/mod.rs
    
    Also the direction of my current solution currently does not preserve the original locations of asteroids, so I need to rework everything to do part 2.

--- 2019/day10/src/main.rs
+++ 2019/day10/src/main.rs
@@ -153,13 +153,120 @@ fn get_num_detectable(map: &AsteroidMap) -> Result<PlacementResult, PlacementErr
     }
 }
 
+fn sort_quadrant<'a, P, F>(asteroids: P, filter: F, flip: bool) -> Vec<(i32, i32)>
+where
+    P: Iterator<Item = &'a (i32, i32)>,
+    F: Fn(i32, i32) -> bool,
+{
+    let mut ret = Vec::new();
+    for &(x, y) in asteroids {
+        if filter(x, y) {
+            ret.push((x, y));
+        }
+    }
+    ret.sort_by(|&(a_x, a_y), &(b_x, b_y)| {
+        if flip {
+            Rational32::new(a_x, a_y).cmp(&Rational32::new(b_x, b_y))
+        } else {
+            Rational32::new(b_x, b_y).cmp(&Rational32::new(a_x, a_y))
+        }
+    });
+    ret
+}
+
+fn sort_asteroids_for_laser(placement: &PlacementResult) -> Vec<(i32, i32)> {
+    let mut ret: Vec<(i32, i32)> = Vec::new();
+
+    // up
+    if placement.asteroids_in_direction.contains_key(&(0, -1)) {
+        ret.push((0, -1));
+    }
+
+    // top right
+    ret.append(&mut sort_quadrant(
+        placement.asteroids_in_direction.keys(),
+        |x, y| x > 0 && y < 0,
+        false,
+    ));
+
+    //right
+    if placement.asteroids_in_direction.contains_key(&(1, 0)) {
+        ret.push((1, 0));
+    }
+
+    // bottom right
+    ret.append(&mut sort_quadrant(
+        placement.asteroids_in_direction.keys(),
+        |x, y| x > 0 && y > 0,
+        false,
+    ));
+
+    // down
+    if placement.asteroids_in_direction.contains_key(&(0, -1)) {
+        ret.push((0, -1));
+    }
+
+    // bottom left
+    ret.append(&mut sort_quadrant(
+        placement.asteroids_in_direction.keys(),
+        |x, y| x < 0 && y < 0,
+        true,
+    ));
+
+    // left
+    if placement.asteroids_in_direction.contains_key(&(-1, 0)) {
+        ret.push((-1, 0));
+    }
+
+    // top left
+    ret.append(&mut sort_quadrant(
+        placement.asteroids_in_direction.keys(),
+        |x, y| x < 0 && y < 0,
+        true,
+    ));
+    ret
+}
+
+fn generate_laser_firing_sequence(place: &PlacementResult) -> Vec<(isize, isize)> {
+    let seq = sort_asteroids_for_laser(&place);
+    let mut counts = place.asteroids_in_direction.clone();
+    let mut ret: Vec<(isize, isize)> = Vec::new();
+    let mut cnt = 0;
+    loop {
+        let mut found = false;
+        for &(x, y) in seq.iter() {
+            let remaining = *counts.get(&(x, y)).unwrap();
+            if remaining > 0 {
+                cnt += 1;
+
+                let translated = (place.x as isize + x as isize, place.y as isize + y as isize);
+
+                println!(
+                    "asteroid {} at ({}, {}), translated: ({}, {})",
+                    cnt, x, y, translated.0, translated.1
+                );
+
+                found = true;
+                ret.push(translated);
+                counts.insert((x, y), remaining - 1);
+            }
+        }
+        if !found {
+            break;
+        }
+    }
+    ret
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let map = AsteroidMap::parse(include_str!("input.txt"))?;
     println!("map is width {} height {}", map.width, map.height);
     let detected = get_num_detectable(&map)?;
     println!(
         "detected at ({},{}): {}",
-        detected.x, detected.y, detected.num_detectable()
+        detected.x,
+        detected.y,
+        detected.num_detectable()
     );
     Ok(())
 }
@@ -272,10 +379,7 @@ mod test {
         assert_eq!(result.num_detectable(), 41);
     }
 
-    #[test]
-    fn test_case5() {
-        let map = AsteroidMap::parse(
-            r#"
+    static BIG_MAP: &'static str = r#"
 .#..##.###...#######
 ##.############..##.
 .#.######.########.#
@@ -296,12 +400,63 @@ mod test {
 .#.#.###########.###
 #.#.#.#####.####.###
 ###.##.####.##.#..##
-"#,
-        )
-        .unwrap();
+"#;
 
+    #[test]
+    fn test_case5() {
+        let map = AsteroidMap::parse(BIG_MAP).unwrap();
         let result = get_num_detectable(&map).unwrap();
         assert_eq!((result.x, result.y), (11, 13));
         assert_eq!(result.num_detectable(), 210);
+    }
+
+    #[test]
+    fn test_sort_quadrant_top_right() {
+        // pre-sorted input
+        let input = [(1, -3), (2, -2), (3, -1)];
+        let result = sort_quadrant(input.iter(), |_, _| true, false);
+        assert_eq!(result, input);
+    }
+
+    #[test]
+    fn test_sort_quadrant_bottom_right() {
+        // pre-sorted input
+        let input = [(3, 1), (2, 2), (1, 3)];
+        let result = sort_quadrant(input.iter(), |_, _| true, false);
+        assert_eq!(result, input);
+    }
+
+    #[test]
+    fn test_sort_quadrant_bottom_left() {
+        // pre-sorted input
+        let input = [(-1, -3), (-2, -2), (-3, -1)];
+        let result = sort_quadrant(input.iter(), |_, _| true, true);
+        assert_eq!(result, input);
+    }
+
+    #[test]
+    fn test_sort_quadrant_top_left() {
+        // pre-sorted input
+        let input = [(-3, 1), (-2, 2), (-1, 3)];
+        let result = sort_quadrant(input.iter(), |_, _| true, true);
+        assert_eq!(result, input);
+    }
+
+    #[test]
+    fn test_generate_laser_firing_sequence() {
+        let map = AsteroidMap::parse(BIG_MAP).unwrap();
+        let place = get_num_detectable(&map).unwrap();
+        assert_eq!((place.x, place.y), (11, 13));
+        let seq = generate_laser_firing_sequence(&place);
+
+        // offset index by one because the test cases are defined in the terms "first", "second" element,
+        // etc.
+        assert_eq!(seq.get(1 - 1).unwrap(), &(11, 12));
+        assert_eq!(seq.get(2 - 1).unwrap(), &(12, 1));
+        assert_eq!(seq.get(3 - 1).unwrap(), &(12, 2));
+        assert_eq!(seq.get(10 - 1).unwrap(), &(12, 8));
+        assert_eq!(seq.get(20 - 1).unwrap(), &(16, 0));
+        assert_eq!(seq.get(50 - 1).unwrap(), &(16, 9));
+        // assert_eq!(seq.get(100 - 1).unwrap(), &(10, 16));
     }
 }
