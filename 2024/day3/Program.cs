39e1ce54ac3b624e22b527049d4c2d0cc1c8c89b
Austin Wise <AustinWise@gmail.com>
2025-01-02 20:21:56 +0000

    2024 day 3 part 2

--- 2024/day3/Program.cs
+++ 2024/day3/Program.cs
@@ -7,16 +7,32 @@ partial class Program
     static void Main()
     {
         string input = File.ReadAllText("input.txt");
+        bool enabled = true;
         long part1 = 0;
+        long part2 = 0;
         foreach (Match m in MulRegex().Matches(input))
         {
-            int x = int.Parse(m.Groups["x"].Value, CultureInfo.InvariantCulture);
-            int y = int.Parse(m.Groups["y"].Value, CultureInfo.InvariantCulture);
-            part1 += x * y;
+            if (m.Value == "do()")
+            {
+                enabled = true;
+            }
+            else if (m.Value == "don't()")
+            {
+                enabled = false;
+            }
+            else
+            {
+                int x = int.Parse(m.Groups["x"].Value, CultureInfo.InvariantCulture);
+                int y = int.Parse(m.Groups["y"].Value, CultureInfo.InvariantCulture);
+                part1 += x * y;
+                if (enabled)
+                    part2 += x * y;
+            }
         }
         Console.WriteLine($"Part 1: {part1}");
+        Console.WriteLine($"Part 2: {part2}");
     }
 
-    [GeneratedRegex(@"mul\((?<x>\d+),(?<y>\d+)\)")]
+    [GeneratedRegex(@"do\(\)|don\'t\(\)|mul\((?<x>\d+),(?<y>\d+)\)")]
     private static partial Regex MulRegex();
 }
