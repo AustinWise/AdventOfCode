153c6a4eb5b0417f88388597aec5c585df8453f7
Austin Wise <AustinWise@gmail.com>
2025-01-02 20:18:50 +0000

    2024 day 3 part 1

---
+++ 2024/day3/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+partial class Program
+{
+    static void Main()
+    {
+        string input = File.ReadAllText("input.txt");
+        long part1 = 0;
+        foreach (Match m in MulRegex().Matches(input))
+        {
+            int x = int.Parse(m.Groups["x"].Value, CultureInfo.InvariantCulture);
+            int y = int.Parse(m.Groups["y"].Value, CultureInfo.InvariantCulture);
+            part1 += x * y;
+        }
+        Console.WriteLine($"Part 1: {part1}");
+    }
+
+    [GeneratedRegex(@"mul\((?<x>\d+),(?<y>\d+)\)")]
+    private static partial Regex MulRegex();
+}
