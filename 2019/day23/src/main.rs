945a64b7586ca6e6e92671d73ecaefcbb254d133
Austin Wise <AustinWise@gmail.com>
2025-01-05 16:59:34 -0800

    2019 day 23 part 2

--- 2019/day23/src/main.rs
+++ 2019/day23/src/main.rs
@@ -1,10 +1,12 @@
 use std::error::Error;
 use std::sync::mpsc::channel;
 use std::sync::mpsc::Receiver;
+use std::sync::mpsc::RecvTimeoutError;
 use std::sync::mpsc::Sender;
 use std::sync::mpsc::TryRecvError;
 use std::sync::LazyLock;
 use std::thread;
+use std::time::Duration;
 
 use intcode::CpuIo;
 use intcode::IntcodeError;
@@ -43,14 +45,16 @@ impl CpuIo for ComputerIo {
             self.incoming_buffer = None;
             Ok(num)
         } else {
-            match self.inbox.try_recv() {
+            // Block a tiny bit before returning -1. Without this each "computer" consumes a lot of
+            // real CPU spinning.
+            match self.inbox.recv_timeout(Duration::from_millis(1)) {
                 Ok(message) => {
                     assert_eq!(self.address, message.address);
                     self.incoming_buffer = Some(message.y);
                     Ok(message.x)
                 }
-                Err(TryRecvError::Empty) => Ok(-1),
-                Err(TryRecvError::Disconnected) => Err(IntcodeError::UserInitiatedExit),
+                Err(RecvTimeoutError::Timeout) => Ok(-1),
+                Err(RecvTimeoutError::Disconnected) => Err(IntcodeError::UserInitiatedExit),
             }
         }
     }
@@ -86,7 +90,7 @@ fn one_computer(address: i64, inbox: Receiver<Message>, outbox: Sender<Message>)
     intcode::execute_with_io(&mut mem, &mut io).unwrap();
 }
 
-fn part_1() {
+fn run_computer_network(enable_nat: bool) {
     let mut computer_inboxes = Vec::new();
     let mut thread_handles = Vec::new();
     let (switch_sender, switch_receiver) = channel::<Message>();
@@ -102,8 +106,42 @@ fn part_1() {
 
     drop(switch_sender);
 
+    let mut nat_value: Option<Message> = None;
+    let mut previously_sent_nat_y_value: Option<i64> = None;
+
     loop {
-        let message = switch_receiver.recv().unwrap();
+        // Ideally this would more preciously measure "idle": all the `ComputerIo` structs have
+        // `incoming_buffer == None` and there are no messages in our switch channel.
+        // Instead we assume that if we have not recieved any messages at the switch for some period
+        // of time, the network is "idle". The larger the receive timeout, the more likly this is true.
+        // It seems to work well enough 10 ms here and having each computer sleep 1 ms before considering
+        // its incoming queue empty.
+        let message = match switch_receiver.recv_timeout(Duration::from_millis(10)) {
+            Ok(message) => message,
+            Err(RecvTimeoutError::Timeout) => {
+                if enable_nat {
+                    if let Some(nat_value) = &nat_value {
+                        println!("sending nat value to address 0: {:?}", nat_value);
+                        if let Some(prev) = previously_sent_nat_y_value {
+                            if prev == nat_value.y {
+                                println!("part 2 answer: {}", prev);
+                                break;
+                            }
+                        }
+                        previously_sent_nat_y_value = Some(nat_value.y);
+                        computer_inboxes[0]
+                            .send(Message {
+                                address: 0,
+                                x: nat_value.x,
+                                y: nat_value.y,
+                            })
+                            .unwrap();
+                    }
+                }
+                continue;
+            }
+            Err(RecvTimeoutError::Disconnected) => panic!("switch disconnected?"),
+        };
         match message.address {
             0..50 => {
                 computer_inboxes[message.address as usize]
@@ -111,9 +149,15 @@ fn part_1() {
                     .unwrap();
             }
             255 => {
-                println!("got part 1 message: {:?}", message);
-                println!("part 1: {}", message.y);
-                break;
+                if enable_nat {
+                    nat_value = Some(message);
+                } else {
+                    println!("got part 1 message: {:?}", message);
+                    println!("part 1: {}", message.y);
+                    println!();
+                    println!();
+                    break;
+                }
             }
             _ => panic!("unexpected address: {}", message.address),
         }
@@ -130,8 +174,7 @@ fn part_1() {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    part_1();
-
-    println!("{}", PROGRAM.len());
+    run_computer_network(false);
+    run_computer_network(true);
     Ok(())
 }
