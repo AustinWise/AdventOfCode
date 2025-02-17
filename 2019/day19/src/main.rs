43c6617a3a672afee22e154d49c3e92a62045b03
Austin Wise <AustinWise@gmail.com>
2025-02-17 15:01:50 -0800

    2029 day 19 part 2

--- 2019/day19/src/main.rs
+++ 2019/day19/src/main.rs
@@ -1,4 +1,7 @@
+use std::cmp::max;
 use std::error::Error;
+
+use owo_colors::OwoColorize;
 
 use intcode::CpuIo;
 use intcode::IntcodeError;
@@ -76,14 +79,74 @@ fn find_starting_point(mem: &Memory) -> Vec2 {
     point
 }
 
-fn part_2(mem: &Memory) {
+#[allow(dead_code)]
+fn print_board(mem: &Memory, left_point: Vec2, right_point: Vec2) {
+    let max_x = max(left_point.x, right_point.x) + 5;
+    let max_y = max(left_point.y, left_point.y) + 5;
+
+    for y in 0..max_y {
+        for x in 0..max_x {
+            let point = Vec2::new(x, y);
+            if in_tractor_beam(mem, point) {
+                if point == left_point {
+                    if point == right_point {
+                        print!("{}", "#".purple());
+                    } else {
+                        print!("{}", "#".blue());
+                    }
+                } else if point == right_point {
+                    print!("{}", "#".red());
+                } else {
+                    print!("#");
+                }
+            } else {
+                print!(".");
+            }
+        }
+        println!();
+    }
+}
+
+fn part_2(mem: &Memory) -> Vec2 {
     let start_point = find_starting_point(mem);
-    println!("start: {:?}", start_point);
+
+    let mut left_point = start_point;
+    let mut right_point = start_point;
+
+    loop {
+        let x_diff = right_point.x - left_point.x;
+        assert!(x_diff >= -1);
+        let y_diff = left_point.y - right_point.y;
+        assert!(y_diff >= -1);
+
+        if x_diff == 99 && y_diff == 99 {
+            assert!(!in_tractor_beam(mem, left_point + Vec2::new(-1, 0)));
+            assert!(!in_tractor_beam(mem, right_point + Vec2::new(1, 0)));
+            return Vec2::new(left_point.x, right_point.y);
+        }
+
+        if x_diff > y_diff {
+            let mut candidate = left_point + Vec2::new(0, 1);
+            while !in_tractor_beam(mem, candidate) {
+                candidate = candidate + Vec2::new(1, 0);
+            }
+            left_point = candidate;
+        } else {
+            let mut candidate = right_point + Vec2::new(1, 0);
+            if in_tractor_beam(mem, candidate) {
+            } else {
+                candidate = right_point + Vec2::new(1, 1);
+                assert!(in_tractor_beam(mem, candidate));
+            }
+            right_point = candidate;
+        }
+    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mem = intcode::parse_program(include_str!("input.txt"))?;
     println!("part 1: {}", part_1(&mem));
-    part_2(&mem);
+    let part_2_res = part_2(&mem);
+    println!("part 2: {}", part_2_res.x * 10000 + part_2_res.y);
     Ok(())
 }
